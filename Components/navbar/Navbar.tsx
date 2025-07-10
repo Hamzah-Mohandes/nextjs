54d70830114b86d6c108490cb89a573d64eb0e07
Hamzah <rootmoodrm@gmail.com>
2025-07-10 20:55:15 +0200

    loading and Template

--- Components/navbar/Navbar.tsx
+++ Components/navbar/Navbar.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import Link from "next/link";
 import React from "react";
-
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 function Navbar() {
-
+    const pathname = usePathname(); // das ist der aktuelle Pfad der Seite
+    const router = useRouter(); // das ist der Router der uns zurueck zur vorherigen Seite führt
     const navs = [
         {
             name: "Home",
@@ -17,19 +21,25 @@ function Navbar() {
             href: "/store"
         }
     ]
-
+    const handelClick = () => {
+        router.back()
+    }
     return (
         <div>
             <nav className="flex justify-start p-4 bg-lime-900 text-white font-bold fixed top-0 w-full">
                 <ul className="flex gap-4">
                     {navs.map((nav) => (
                         <li key={nav.name}>
-                            <Link href={nav.href} className="hover:text-lime-500 transition-colors">{nav.name}</Link>
+                            <Link href={nav.href} className={pathname === nav.href ? "text-lime-500" : "hover:text-lime-500 transition-colors  "}>{nav.name}</Link>
                         </li>
+
                     ))}
                 </ul>
+                <button className="ml-auto " onClick={handelClick}>
+                    Back
+                </button>
             </nav>
-        </div>
+        </div >
     )
 }
 
